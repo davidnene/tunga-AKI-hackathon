dcd9021dc1495f37055af7177d0e9cf59e778b8d
Lubanga Derrick <derrick.lubanga@strathmore.edu>
2024-09-18 13:49:31 +0300

    Update agent.py
    Updated gpt version

--- agent.py
+++ agent.py
@@ -25,7 +25,7 @@ def create_agent(filename: str):
     """
 
     # Create an OpenAI object.
-    llm = OpenAI(model_name = 'gpt-4o')
+    llm = OpenAI(model_name = 'gpt-4-turbo')
 
     # Read the CSV file into a Pandas DataFrame.
     df = pd.read_csv(filename)
